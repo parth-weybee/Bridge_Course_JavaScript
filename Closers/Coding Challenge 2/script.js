258eba712152d8a7dc007367b2b7bcf694b5a020
parth-weybee <parthmodhvadiya@weybee.in>
2025-08-25 18:21:36 +0530

    Closer Coding Challenge 2 Completed

---
+++ Closers/Coding Challenge 2/script.js
@@ -0,0 +1,7 @@
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+    document.body.addEventListener('click', function () {
+        header.style.color = 'blue';
+    });
+})();
